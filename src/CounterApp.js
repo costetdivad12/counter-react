b32ad2cd698e3d97209f8afcc7efb265a2137efb
costetdivad12 <costet910405@outlook.es>
2022-05-08 18:52:37 -0500

    counter terminado

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState} from "react";
 import PropTypes from 'prop-types'
 const CounterApp = ({valor}) => {
-    // console.log(props);
- 
-    // const saludo='hola mundo ';
-    // const saludo= {
-    //    nombre:'David Costet',
-    //    edad: 30
-    // };
+   
+  const [counter,setCounter]= useState(valor);
+  
+  // handleadd
+  const handleadd= ()=>{
+    setCounter(counter+1);
+  }
+  const handlerest= ()=>{
+    setCounter(counter-1);
+  }
+
+  const handlereset = ()=>{
+    setCounter(valor);
+  }
+
   return (
     <>
-      {/* <pre> {JSON.stringify(saludo) } </pre>  */}
+
       <h1>CounterApp</h1>
-      <p> {valor} </p>
+      <p> {counter} </p>
+      <button onClick={ handleadd }>+1</button>
+      <button onClick={ handlereset }>reset</button>
+      <button onClick={ handlerest }>-1</button>
+
     </>
   );
 };
